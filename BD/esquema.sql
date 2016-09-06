99d3589ff85a2671c84287f1494f5bc6fc642357
CharlesGarrocho <charles.garrocho@gmail.com>
2016-09-05 22:09:28 -0300

    alterado: esquema.

--- BD/esquema.sql
+++ BD/esquema.sql
@@ -1,33 +1,63 @@
-DROP TABLE IF EXISTS Programador;
+DROP TABLE IF EXISTS Pessoa;
 
-DROP TABLE IF EXISTS Programa;
+DROP TABLE IF EXISTS Aviao;
 
-DROP TABLE IF EXISTS Analista;
+DROP TABLE IF EXISTS Classe;
 
-DROP TABLE IF EXISTS Faz;
+DROP TABLE IF EXISTS Assento;
 
-CREATE TABLE Programador (
-	Codigo integer PRIMARY KEY AUTOINCREMENT,
-	Nome text
+DROP TABLE IF EXISTS Voo;
+
+DROP TABLE IF EXISTS Aeroporto;
+
+DROP TABLE IF EXISTS Passagem;
+
+DROP TABLE IF EXISTS Refeicao;
+
+CREATE TABLE Pessoa (
+	Identidade string,
+	Nome string
 );
 
-CREATE TABLE Programa (
-	Codigo integer PRIMARY KEY AUTOINCREMENT,
-	Nome text
+CREATE TABLE Aviao (
+	NumeroSerie string,
+	Modelo string
 );
 
-CREATE TABLE Analista (
+CREATE TABLE Classe (
 	Codigo integer PRIMARY KEY AUTOINCREMENT,
-	Nome text
+	Nome string,
+	Refeicao integer
 );
 
-CREATE TABLE Faz (
-	CodigoProgramador integer,
-	CodigoPrograma integer,
-	CodigoAnalista integer,
-	Nota integer,
-	FOREIGN KEY(CodigoProgramador) REFERENCES Programador(Codigo),
-	FOREIGN KEY(CodigoPrograma) REFERENCES Programa(Codigo),
-	FOREIGN KEY(CodigoAnalista) REFERENCES Analista(Codigo)
+CREATE TABLE Assento (
+	Numero integer PRIMARY KEY AUTOINCREMENT,
+	Classe integer
+);
+
+CREATE TABLE Voo (
+	Codigo integer PRIMARY KEY AUTOINCREMENT,
+	Aviao string,
+	Piloto string,
+	Origem string,
+	Destino string,
+	HorarioPartida timestamp,
+	HorarioChegada timestamp
+);
+
+CREATE TABLE Aeroporto (
+	Codigo string,
+	Nome string
+);
+
+CREATE TABLE Passagem (
+	Passageiro string,
+	Voo string,
+	Assento integer
+);
+
+CREATE TABLE Refeicao (
+	Codigo integer PRIMARY KEY AUTOINCREMENT,
+	Nome string
 );
 
